f19c821769c85d2f58315e7e7dc78ef78196913c
Jefferson Vilela <Jefferson Vilela@DESKTOP-D5NELOG>
2025-11-06 10:27:24 -0300

    add recursos ao app

--- MauiAppHotel/Protegida.xaml.cs
+++ MauiAppHotel/Protegida.xaml.cs
@@ -5,5 +5,22 @@ public partial class Protegida : ContentPage
 	public Protegida()
 	{
 		InitializeComponent();
-	}
+		string? usuario_logado = null;
+			
+			Task.Run(async () =>
+			{ 
+				usuario_logado = await SecureStorage.Default.GetAsync("usuario_logado");
+				lbl_boasvindas.Text = $"Seja bem-vindo, {usuario_logado}!";
+            });
+}
+	private async void Button_Clicked(object sender, EventArgs e)
+	{
+		bool confirm = await DisplayAlert("Tem Certeza?", "Deseja realmente sair?", "Sim", "Não");
+		if (confirm)
+		{
+			SecureStorage.Default.Remove("usuario_logado");
+			App.Current.MainPage = new Login();
+            
+        }
+    }
 }

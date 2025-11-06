9e9853a1f0eb11f2fbcd9e8625bf8a632e9706f1
Jefferson Vilela <Jefferson Vilela@DESKTOP-D5NELOG>
2025-11-06 09:22:23 -0300

    add recursos ao app

---
+++ MauiAppHotel/DadosUsuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppHotel
+{
+    public class DadosUsuario
+    {
+        public string Usuario { get; set; }
+
+        public string Senha { get; set; }
+
+
+    }
+}
--- MauiAppHotel/Login.xaml.cs
+++ MauiAppHotel/Login.xaml.cs
@@ -1,9 +1,56 @@
 namespace MauiAppHotel;
-
 public partial class Login : ContentPage
 {
-	public Login()
-	{
-		InitializeComponent();
-	}
-}
+    public Login()
+    {
+        InitializeComponent();
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            List<DadosUsuario> lista_Usuarios = new List<DadosUsuario>
+            {
+                new DadosUsuario()
+                {
+                    Usuario = "admin",
+                    Senha = "admin"
+                }
+            };
+
+            DadosUsuario dados_digitados = new()
+            {
+                Usuario = txt_usuario.Text,
+                Senha = txt_senha.Text
+            };
+
+            // LINQ para verificar se o usuário existe na lista
+            if (lista_Usuarios.Any(u => u.Usuario == dados_digitados.Usuario && u.Senha == dados_digitados.Senha))
+            {
+                await SecureStorage.Default.SetAsync("usuario_logado", dados_digitados.Usuario);
+
+               
+                var window = Application.Current?.Windows.FirstOrDefault();
+                if (window != null)
+                {
+                    window.Page = new Protegida();
+                }
+                else
+                {
+                    throw new Exception("Nenhuma janela ativa encontrada para alterar a página.");
+                }
+            }
+            else
+            {
+                throw new Exception("Usuário ou senha inválidos!");
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", ex.Message, "Fechar");
+        }
+    }
+
+
+}// fecha class
